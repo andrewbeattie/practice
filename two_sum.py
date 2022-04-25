e692f8eedfd54ab04412b955162675e8de70208c
Andrew <andrew.beattie@farmersedge.ca>
2022-04-25 12:20:22 -0600

    mapping approach implementation

--- two_sum.py
+++ two_sum.py
@@ -1,14 +1,21 @@
 from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            val = target - nums[i]
-            if val in nums:
-                if i != nums.index(val):
-                    sums = [i, nums.index(val)]
-                    sums.sort()
-                    return sums
+        num_map = {}
+        for i, num in enumerate(nums):
+            if target - num in num_map:
+                return [num_map[target - num], i]
+            else:
+                num_map[num] = i
         return []
+        # for i in range(len(nums)):
+        #     val = target - nums[i]
+        #     if val in nums:
+        #         if i != nums.index(val):
+        #             sums = [i, nums.index(val)]
+        #             sums.sort()
+        #             return sums
+        # return []
         # for i, n in enumerate(nums):
         #     initial = target - n
         #     if initial in nums:
